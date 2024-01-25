1b37567638bdc475dad1ce61049361355e52c6bf
Omor Riduan <93811768+smRid@users.noreply.github.com>
2024-01-25 13:05:30 +0600

    update js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ $('.main_banner_slider').slick({
     speed: 1000,
     fade: true,
 });
+
+$('.main_slider').slick({
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: `<i class="fa-solid fa-angle-left left"></i>`,
+    nextArrow: `<i class="fa-solid fa-angle-right right"></i>`,
+    centerMode: true,
+    centerPadding: '0px',
+});
